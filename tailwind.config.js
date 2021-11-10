8e7a9d3af72a02b2bb17a2a9b3360d912430d766
Rakhimov Kamran <rakhimovkamran@gmail.com>
2021-11-11 03:15:17 +0500

    | Tailwind | Update ( Config )

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    purge: ["./components//*.{js,ts,jsx,tsx}", "./pages//*.{js,ts,jsx,tsx}"],
+    purge: [
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./sections/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {},
     },
